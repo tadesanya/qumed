a7b8d95be8e73523e53950482893fe65f3cc075a
andela-tadesanya <adeyemi.adesanya@andela.com>
2018-02-26 14:40:53 +0100

    setup referral app

---
+++ qumed/qumed/constants.py
@@ -0,0 +1,10 @@
+# character lengths
+char_length_16 = 16
+char_length_32 = 32
+char_length_128 = 128
+
+APPOINTMENT_STATUS = (
+    ('scheduled', 'SCHEDULED'),
+    ('complete', 'COMPLETE'),
+    ('lmts', 'LMTS')
+)
---
+++ qumed/referral/models.py
@@ -0,0 +1,38 @@
+import uuid
+
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+from qumed.constants import char_length_16, char_length_32, char_length_128, APPOINTMENT_STATUS
+
+
+class Practice(models.Model):
+    name = models.CharField(max_length=char_length_32, unique=True)
+    address = models.CharField(max_length=char_length_128)
+    email = models.EmailField()
+    telephone = PhoneNumberField()
+
+
+class Patient(models.Model):
+    mrn = models.IntegerField(unique=True)
+    name = models.CharField(max_length=char_length_32, unique=True)
+    address = models.CharField(max_length=char_length_128)
+    email = models.EmailField()
+    telephone = PhoneNumberField()
+
+
+class Referral(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    patient = models.ForeignKey(Patient)
+    status = models.CharField(max_length=char_length_16, choices=APPOINTMENT_STATUS)
+    notes = models.TextField()
+
+    date_referred = models.DateField(auto_now_add=True)
+    referred_by = models.ForeignKey(Practice)
+    referred_to = models.ForeignKey(Practice)
+    reason_for_referral = models.CharField(max_length=char_length_128)
+
+    first_attempt = models.DateTimeField(null=True, blank=True)
+    second_attempt = models.DateTimeField(null=True, blank=True)
+    third_attempt = models.DateTimeField(null=True, blank=True)
+    appointment_date = models.DateTimeField(null=True, blank=True)
