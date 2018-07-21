6edf8aa1f36a10f52af2438bc60d732c08ea3d6a
andela-tadesanya <adeyemi.adesanya@andela.com>
2018-07-21 19:33:36 +0100

    create appointment model

--- qumed/referral/admin.py
+++ qumed/referral/admin.py
@@ -12,12 +12,15 @@ class PatientAdmin(admin.ModelAdmin):
 
 class ReferralAdmin(admin.ModelAdmin):
     list_display = ('id', 'patient', 'date_referred', 'reason_for_referral', 'notes', 'referral_status', 'referred_by',
-                    'referred_to', 'appointment_status', 'first_attempt', 'second_attempt', 'third_attempt',
-                    'appointment_date')
+                    'referred_to')
 
 class TempReferralAdmin(admin.ModelAdmin):
     list_display = ('id', 'patient', 'date_referred', 'reason_for_referral', 'notes', 'referred_by',
                     'referred_to_email')
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('appointment_status', 'first_attempt', 'second_attempt', 'third_attempt', 'appointment_date',
+                    'patient', 'practice', 'physician_seen')
 
 
 admin.site.register(Practice, PracticeAdmin)
